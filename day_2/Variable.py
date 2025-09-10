23e5a94b3d46b6b5ba3ecf5b4250ed52d03d657d
NotStrix <rosariojose7505@gmail.com>
2025-09-10 18:13:20 -0400

    Fix: remove submodule and add actual Python files

---
+++ day_2/Variable.py
@@ -0,0 +1,18 @@
+age = 19
+
+print("Hello, World!")
+print("My age is", age, "years old.")
+
+pi = 3.14159
+print("Pi:", pi)
+
+name = "Jose"
+print("Hello,", name)
+
+is_student = False
+print("Is student:", is_student)
+
+print(type(age))
+print(type(pi))
+
+print(type(name))
