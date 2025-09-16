f2d125eebe7ee60e700a5431d057634bd0f1bd7c
NotStrix <rosariojose7505@gmail.com>
2025-09-16 19:52:25 -0400

    Learned to use/create lists and implemented previous learned input to add integers to the list.

--- day_6/lists.py
+++ day_6/lists.py
@@ -1,8 +1,7 @@
 # Jose Rosario
 # Lists task
 # Day 6
-
-numbers = [10, 4 , 8, 11, 12, 3, 7, 2, 9, 5]
+numbers = [10, 4 , 28, 8, 11, 12, 3, 7, 2, 9, 5]
 
 # Print Numbers
 for num in numbers:
@@ -17,5 +16,5 @@ numbers.remove(int(input("Enter a number to remove from the list: ")))
 # Sort Numbers
 numbers.sort()
 for num in numbers:
-    if num % 2 ==0:
+    if num % 2 == 0:
         print("Even number:", num)
