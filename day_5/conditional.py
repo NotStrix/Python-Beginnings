988ad20a49a84af9ea43557c76e9153ad0531be9
NotStrix <rosariojose7505@gmail.com>
2025-09-16 19:50:40 -0400

    Learned to use conditionals to grade assignments based off a number grade

---
+++ day_5/conditional.py
@@ -0,0 +1,17 @@
+# Jose Rosario
+# Conditonal Statemnts task
+# Day 5
+
+grade = int(input("Please enter number grade for assignment: "))
+
+if grade >= 90:
+    print("You received an A")
+elif grade >= 80:
+    print("You received a B")   
+elif grade >= 70:
+    print("You received a C")
+elif grade >= 65:
+    print("You received a D")
+else:
+    print("You received an F")
+    
---
+++ day_6/lists.py
@@ -0,0 +1,21 @@
+# Jose Rosario
+# Lists task
+# Day 6
+
+numbers = [10, 4 , 8, 11, 12, 3, 7, 2, 9, 5]
+
+# Print Numbers
+for num in numbers:
+    print(num)
+
+# Add Number
+numbers.append(int(input("Enter a number to add to the list: ")))
+
+# Remove Number
+numbers.remove(int(input("Enter a number to remove from the list: ")))
+
+# Sort Numbers
+numbers.sort()
+for num in numbers:
+    if num % 2 ==0:
+        print("Even number:", num)
